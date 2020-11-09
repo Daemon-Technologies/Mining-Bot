2f3bfe9f7b1db1976c28e820138171376ea5e272
SherLzp <lzp.sher@gmail.com>
2020-11-09 20:51:36 +0800

    [Sher] add two languages switch:en-US and zh-CN

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { DefaultFooter } from '@ant-design/pro-layout';
+import { getLanguage } from '@ant-design/pro-layout/lib/locales';
+
+const { CN } = require('@/services/constants');
 
 export default () => (
   <DefaultFooter
-    copyright="2020 BlockStack"
+    copyright='2020 BlockStack'
     links={[
       {
-        key: 'Mining Bot',
-        title: 'Mining Bot',
+        key: getLanguage() === CN ? '挖矿机器人' : 'Mining Bot',
+        title: getLanguage() === CN ? '挖矿机器人' : 'Mining Bot',
         href: '',
         blankTarget: true,
       },
