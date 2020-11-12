77e218897f9d1fefd33af8d61f465c0dba8bdde5
SherLzp <lzp.sher@gmail.com>
2020-11-12 18:42:13 +0800

    [Sher] fix bug: when type wrong password at unlock page, then type the correct password, still unlock fail.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,8 +6,7 @@ import { message } from 'antd';
 
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
-const { sha256, hexToken, shaSecret, aes256Gcm, aesKeySize, sha512 } = require('@/services/constants')
-const hmac = crypto.createHmac(sha256, shaSecret);
+const { sha256, hexToken, shaSecret, aes256Gcm, aesKeySize } = require('@/services/constants')
 
 export const isUrl = (path: string): boolean => reg.test(path);
 
@@ -51,6 +50,7 @@ export function coerceAddress(address: string) {
 }
 
 export function getShaValue(data: string) {
+  const hmac = crypto.createHmac(sha256, shaSecret);
   return hmac.update(data).digest(hexToken);
 }
 
