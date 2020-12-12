c576c1462e6b551954bc4a9d46ef1313d3b86daf
SherLzp <lzp.sher@gmail.com>
2020-12-12 16:27:00 +0800

    [Sher] fix locale Warning problem and change getLanguage to getLocale

---
+++ src/pages/wallet/locales/en-US.ts
@@ -0,0 +1,17 @@
+export default {
+    'account.title': 'Account Info',
+    'account.address': 'Address',
+    'account.type': 'Type',
+    'account.balance': 'Balance',
+    'account.add': 'Add',
+    'account.choose': 'Already choose',
+
+    'faucet.get' : 'Get Faucet',
+    'faucet.add' : 'Get Faucet',
+    'faucet.notification': 'Faucet Confirm',
+    'faucet.notification.content': 'If you want to get Faucet for address : ',
+
+    'choose.num': 'items',
+
+    'button.delete' : 'Delete'
+};
